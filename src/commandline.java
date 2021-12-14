0591b62fe2d2b3218bac520ab443c1ea9609a105
Priyaksm12 <priyankaks.priya@gmail.com>
2021-12-14 21:25:57 +0530

    day1 assignment

---
+++ src/commandline.java
@@ -0,0 +1,12 @@
+public class commandline {
+
+        static void myMethod(String fname) {
+            System.out.println(fname + " KS");
+        }
+
+        public static void main(String[] args) {
+            myMethod("Priya");
+
+
+        }
+    }
